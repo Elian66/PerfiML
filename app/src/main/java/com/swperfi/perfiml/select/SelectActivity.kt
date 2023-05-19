2c08f2fb5e0689d83c261687ea64e3c29e6896d7
romul <romulo.junior@icomp.ufam.edu.br>
2023-05-19 15:21:09 -0400

    criação da primeira versão da tela inicial com um botão

--- app/src/main/java/com/swperfi/perfiml/select/SelectActivity.kt
+++ app/src/main/java/com/swperfi/perfiml/select/SelectActivity.kt
@@ -2,10 +2,19 @@ package com.swperfi.perfiml.select
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.swperfi.perfiml.R
 
 class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select)
+
+        val button: Button = findViewById(R.id.selecioneArquivo)
+
+        button.setOnClickListener {
+            Toast.makeText(this,"texto qualquer",Toast.LENGTH_SHORT).show()
+        }
     }
 }
