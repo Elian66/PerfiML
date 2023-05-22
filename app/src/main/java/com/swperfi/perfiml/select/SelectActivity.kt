94598cd231f9b6591b660dba9ed02291c1af0eb3
romul <romulo.junior@icomp.ufam.edu.br>
2023-05-22 15:50:36 -0400

    Adição de leitura de arquivo e verificação se o arquivo é válido (csv)

--- app/src/main/java/com/swperfi/perfiml/select/SelectActivity.kt
+++ app/src/main/java/com/swperfi/perfiml/select/SelectActivity.kt
@@ -1,10 +1,17 @@
 package com.swperfi.perfiml.select
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.OpenableColumns
 import android.widget.Button
 import android.widget.Toast
 import com.swperfi.perfiml.R
+import java.io.BufferedReader
+import java.io.InputStream
+import java.io.InputStreamReader
 
 class SelectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,7 +21,62 @@ class SelectActivity : AppCompatActivity() {
         val button: Button = findViewById(R.id.selecioneArquivo)
 
         button.setOnClickListener {
-            Toast.makeText(this,"texto qualquer",Toast.LENGTH_SHORT).show()
+            escolherArquivo()
         }
+    }
+
+    private fun escolherArquivo() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "*/*"
+
+        }
+        startActivityForResult(intent, 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+
+            val selectedFile = data?.data
+            if(selectedFile != null){
+                val contentResolver = contentResolver
+                val inputStream = contentResolver.openInputStream(selectedFile)
+                if(isCsvValid(inputStream, "battery_power", "foreground_app")){
+                    val filename = getFileNameFromUri(selectedFile)
+                    Toast.makeText(this, "Arquivo" + filename, Toast.LENGTH_SHORT).show()
+                }
+                else{
+                    Toast.makeText(this, "Arquivo Inválido!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    private fun isCsvValid(inputStream: InputStream?, vararg columns : String): Boolean {
+        inputStream?.let {
+            val reader = BufferedReader(InputStreamReader(inputStream))
+            val header = reader.readLine()
+            if(header != null){
+                val headers = header.split(",")
+                if(headers.containsAll(columns.toList())){
+                    return true
+                }
+            }
+        }
+        return false
+    }
+
+    private fun getFileNameFromUri(uri: Uri): String? {
+        val cursor = contentResolver.query(uri, null, null, null, null)
+        cursor?.let {
+            if (it.moveToFirst()) {
+                val displayNameIndex = it.getColumnIndexOrThrow(OpenableColumns.DISPLAY_NAME)
+                val displayName = it.getString(displayNameIndex)
+                it.close()
+                return displayName
+            }
+        }
+        return null
     }
 }
